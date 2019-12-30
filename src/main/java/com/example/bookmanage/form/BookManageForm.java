6cd0ce9be1ff5d31f133dce655063fbab4b3d2da
raikom123 <raikom99@gmail.com>
2019-12-30 18:16:53 +0900

    データが登録されている場合のテストを追加。

--- src/main/java/com/example/bookmanage/form/BookManageForm.java
+++ src/main/java/com/example/bookmanage/form/BookManageForm.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.example.bookmanage.domain.Book;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class BookManageForm {
 
     /**
